f4204a52fb0341db1596d77c139c88786ee25c30
guilherme <guilherme_ecb@hotmail.com>
2021-04-21 15:40:57 -0300

    fix: remove build

--- build/jest-integration-config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-var config = require('./jest.config');
-config.testMatch = ['**/__tests__/integration/*test.ts'];
-module.exports = config;
